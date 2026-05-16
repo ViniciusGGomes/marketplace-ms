b283df3bbdbe7ae205dfa948a37ee391ba354cc8
vinicius <viniciusgmelllo@gmail.com>
2026-05-15 23:37:44 -0300

    chore(proxy): import FallbackModule into ProxyModule

--- api-gateway/src/proxy/proxy.module.ts
+++ api-gateway/src/proxy/proxy.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ProxyService } from './service/proxy.service';
 import { HttpModule } from '@nestjs/axios';
 import { CircuitBreakerModule } from 'src/common/circuit-breaker/circuit-breaker.module';
+import { FallbackModule } from 'src/common/fallback/fallback.module';
 @Module({
-  imports: [HttpModule, CircuitBreakerModule],
+  imports: [HttpModule, CircuitBreakerModule, FallbackModule],
   providers: [ProxyService],
   exports: [ProxyService],
 })
