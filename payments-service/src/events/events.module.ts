6193e409e4995e345c534f119afd965f9abde6b3
vinicius <viniciusgmelllo@gmail.com>
2026-05-21 23:46:24 -0300

    feat(payments): register PaymentConsumerService in EventsModule

--- payments-service/src/events/events.module.ts
+++ payments-service/src/events/events.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { RabbitMQService } from './rabbitmq/rabbitmq.service';
 import { PaymentQueueService } from './payment-queue/payment-queue.service';
 import { ConfigModule } from '@nestjs/config';
+import { PaymentConsumerService } from './payment-consumer/payment-consumer.service';
 
 @Module({
   imports: [ConfigModule],
-  providers: [RabbitMQService, PaymentQueueService],
+  providers: [RabbitMQService, PaymentQueueService, PaymentConsumerService],
   exports: [RabbitMQService, PaymentQueueService],
 })
 export class EventsModule {}
