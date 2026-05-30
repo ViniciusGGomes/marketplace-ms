3a5e2bfd78c1f1821ba5a1f7f4f8e2fd16aaedc2
vinicius <viniciusgmelllo@gmail.com>
2026-05-29 23:47:34 -0300

    docs(swagger): configure openapi document builder in main setup

--- users-service/src/main.ts
+++ users-service/src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { DocumentBuilder } from 'node_modules/@nestjs/swagger/dist/document-builder';
+import { SwaggerModule } from 'node_modules/@nestjs/swagger/dist/swagger-module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -15,9 +20,19 @@ async function bootstrap() {
     }),
   );
 
+  const config = new DocumentBuilder()
+    .setTitle('Users Service')
+    .setDescription('Microservice de gerenciamento de usuários')
+    .setVersion('1.0')
+    .addBearerAuth() // Suporte ao Token JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT || 3000;
   await app.listen(process.env.PORT ?? port);
 
   console.log(`👤 Users Service running on port ${port}`);
 }
-bootstrap();
+void bootstrap();
