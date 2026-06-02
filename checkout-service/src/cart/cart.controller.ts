7a95c328e12bc4b6dd423c8b947e906627ff1acb
vinicius <viniciusgmelllo@gmail.com>
2026-06-01 22:19:28 -0300

    feat(cart): implement controller endpoints with secure id injection and uuid validation

---
+++ checkout-service/src/cart/cart.controller.ts
@@ -0,0 +1,38 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+} from '@nestjs/common';
+import { CartService } from './cart.service';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { AddCartItemDto } from './dtos/add-cart-item.dto';
+
+@Controller('cart')
+export class CartController {
+  constructor(private readonly cartService: CartService) {}
+
+  @Post('items')
+  async addItem(
+    @CurrentUser('id') userId: string,
+    @Body() dto: AddCartItemDto,
+  ) {
+    return await this.cartService.addItem(userId, dto);
+  }
+
+  @Get()
+  getCar(@CurrentUser('id') userId: string) {
+    return this.cartService.getCart(userId);
+  }
+
+  @Delete('items/:itemId')
+  removeItem(
+    @CurrentUser() userId: string,
+    @Param('itemId', ParseUUIDPipe) itemId: string,
+  ) {
+    return this.cartService.removeItem(userId, itemId);
+  }
+}
