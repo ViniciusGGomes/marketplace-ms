6907fff61a24a50aaa33c6d82d2e06bc95a0edf7
vinicius <viniciusgmelllo@gmail.com>
2026-06-01 22:23:33 -0300

    feat(orders): wire orders module with typeorm feature injection, cross-module contexts and amqp providers

---
+++ checkout-service/src/orders/orders.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { OrdersController } from './orders.controller';
+import { OrdersService } from './orders.service';
+import { CartModule } from '../cart/cart.module';
+import { EventsModule } from '../events/events.module';
+import { PaymentResultConsumerService } from './payment-result-consumer/payment-result-consumer.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Order]), CartModule, EventsModule],
+  controllers: [OrdersController],
+  providers: [OrdersService, PaymentResultConsumerService],
+})
+export class OrdersModule {}
