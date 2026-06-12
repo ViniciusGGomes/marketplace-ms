793fbd12400299227075a559fe02253102d141a9
vinicius <viniciusgmelllo@gmail.com>
2026-06-12 11:18:54 -0300

    fix(payments): fix payment routing and parameter binding for orderId lookup

--- payments-service/src/payments/payments.controller.ts
+++ payments-service/src/payments/payments.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 
-@Controller('payments')
+@Controller()
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
-  @Get(':orderId')
+  @Get('payments/:orderId')
   async findOrderId(@Param('orderId') orderId: string) {
     return await this.paymentsService.findOrderId(orderId);
   }
